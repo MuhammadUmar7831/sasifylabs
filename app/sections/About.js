4c059e019b9f7f7e7dde2cd2e31aec4dfb345b73
MuhammadUmar7831 <mu8494759@gmail.com>
2024-10-02 04:45:55 +0500

    apos

--- app/sections/About.js
+++ app/sections/About.js
@@ -11,18 +11,18 @@ export default function About() {
         We help entrepreneurs and business leaders build and launch innovative
         custom software solutions to the market across a range of verticals &
         industries. We can help you turn your vision into a successful product
-        together, whether it&aposs in the form of a Minimum Viable Product, UX/UI
-        services, full product custom software development or help to
+        together, whether it&aposs in the form of a Minimum Viable Product,
+        UX/UI services, full product custom software development or help to
         scale your company.
       </p>
       <p class="mx-auto text-center text-lg mt-5 w-[1151px]">
         <strong>Vision:</strong> We aim to be the global partner of choice,
         transforming ideas into leading digital products that advance industries
         and drive innovation.
-        <strong>Mission:</strong> We empower startups to launch market-ready
-        SaaS products swiftly. Our MVP Launchpad Package, tailored for Silicon
-        Valley&aposs innovators and beyond, ensures timely, budget-aligned launches
-        without scope creep.
+        <strong>Mission:</strong>{" "}
+        {
+          "We empower startups to launch market-ready SaaS products swiftly. Our MVP Launchpad Package, tailored for Silicon Valley's innovators and beyond, ensures timely, budget-aligned launches without scope creep."
+        }
       </p>
       <div class="flex justify-center gap-10 mt-10">
         <button class="btn">Realize Your Vision</button>
--- app/sections/Awards.js
+++ app/sections/Awards.js
@@ -8,9 +8,9 @@ export default function Awards() {
         <span className="textGradient"> & Recognition</span>
       </h1>
       <p className="headingBottomText w-[1016px]">
-        Celebrated for excellence in digital product development, we&aposve earned
-        top honors from industry leaders. Explore our accolades that showcase
-        our dedication to web and mobile innovation.
+        {
+          "Celebrated for excellence in digital product development, we've earned top honors from industry leaders. Explore our accolades that showcase our dedication to web and mobile innovation."
+        }
       </p>
       <div className="flex gap-5 flex-wrap justify-center mt-10 grayscale filter">
         <div className="glassmorphism rounded-md w-[180px] p-4">
